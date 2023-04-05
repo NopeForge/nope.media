2915d07ab0302032f181218810c3ce4cbb38c053
Clément Bœsch <u@pkh.me>
2023-04-05 13:24:57 +0200

    tests: replace "sfx" with "nmd"
    "sfx" refers to Stupeflix, "nmd" refers to nope.media.

--- tests/test_audio.c
+++ tests/test_audio.c
@@ -33,7 +33,7 @@ int main(int ac, char **av)
                 printf("null frame\n");
                 break;
             }
-            printf("frame #%d / data:%p ts:%f nb_samples:%d sfxsmpfmt:%d\n",
+            printf("frame #%d / data:%p ts:%f nb_samples:%d nmdsmpfmt:%d\n",
                    i++, frame->datap[0], frame->ts, frame->nb_samples, frame->pix_fmt);
             smp += frame->nb_samples;
 
