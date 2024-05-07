3ca8d2f9cd8d0ea281faf3eccaeaf6cdd6e3e4cc
Matthieu Bouron <matthieu.bouron@gmail.com>
2024-05-07 09:41:47 +0200

    tests: fix nmd_create() return check
    Fixes CID 469331, 469328.

--- tests/test_high_refresh_rate.c
+++ tests/test_high_refresh_rate.c
@@ -14,13 +14,13 @@ int main(int ac, char **av)
     const int use_pkt_duration = ac > 2 ? atoi(av[2]) : 0;
 
     struct nmd_ctx *s = nmd_create(filename);
+    if (!s)
+        return -1;
+
     nmd_set_option(s, "auto_hwaccel", 0);
     nmd_set_option(s, "use_pkt_duration", use_pkt_duration);
     struct nmd_frame *f;
     const double t = 1/60.;
-
-    if (!s)
-        return -1;
 
     int ret = nmd_get_frame(s, 0., &f);
     if (ret != NMD_RET_NEWFRAME)
