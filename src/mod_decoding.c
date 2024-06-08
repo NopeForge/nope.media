f6552e9814e0e4b05c01d4c91e8c431520038099
Matthieu Bouron <matthieu.bouron@gmail.com>
2024-06-08 10:26:36 +0200

    build: require FFmpeg >= 6.1
    The latest Ubuntu LTS release (24.04) ships with FFmpeg 6.1, so we can depend
    on it for the time being.

--- src/mod_decoding.c
+++ src/mod_decoding.c
@@ -30,11 +30,7 @@
 
 static void nmi_channel_layout_describe(const AVCodecParameters *par, char *buf, size_t buf_size)
 {
-#if LIBAVUTIL_VERSION_INT < AV_VERSION_INT(57, 24, 100)
-    av_get_channel_layout_string(buf, buf_size, 0, par->channel_layout);
-#else
     av_channel_layout_describe(&par->ch_layout, buf, buf_size);
-#endif
 }
 
 extern const struct decoder nmdi_decoder_ffmpeg_sw;
